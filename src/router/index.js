35188eac26af7167e8bd00570c04f26b7048de5a
TomofumiYabu <ky12088@kyotoss.co.jp>
2019-12-04 20:41:56 +0900

    画像表示ページの枠を作成

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,7 @@ const routes = [
     path: '/addresses',
     name: 'addresses',
     component: Addresses
-  },  
+  },
   {
     path: '/addresses/:address_id?/edit',
     name: 'addresses_edit',
@@ -29,6 +29,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/image',
+    name: 'image',
+    component: () => import(/* webpackChunkName: "image" */ '../views/Image.vue')
   }
 ]
 
---
+++ src/views/Image.vue
@@ -0,0 +1,13 @@
+<template>
+<v-container text-xs-center justify-center>
+  <v-row align="center" justify="center">
+    <v-img
+      src="https://picsum.photos/id/11/500/300"
+      aspect-ratio="1"
+      class="grey lighten-2"
+      max-width="500"
+      max-height="300"
+    ></v-img>
+  </v-row>
+</v-container>
+</template>
